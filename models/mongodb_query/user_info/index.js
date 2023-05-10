a8a31ed3d665a80b72df0deeb2c113cd5aa53b7c
dlawhdgus <212whdgus@gmail.com>
2023-05-10 15:22:14 +0900

    aggregate query

--- models/mongodb_query/user_info/index.js
+++ models/mongodb_query/user_info/index.js
@@ -18,3 +18,45 @@ exports.INSERT_USER_DATA = async (userfilter) => {
         if (e) throw e
     }
 }
+
+exports.GET_USER_ALL_DATA = async (id) => {
+    try {
+        user_info.aggregate([
+            {
+                $lookup: {
+                    from: "more_user_info",
+                    let: { id: "$id" },
+                    pipeline: [
+                      {
+                        $match: {
+                          $expr: { $eq: ["$id", "$$id"] }  //user_info_id, more_user_info_id
+                        }
+                      },
+                      {
+                        $project: {
+                          _id: 0,
+                          email: 1,
+                          phone_number: 1,
+                          address: 1,
+                          flag: 1
+                        }
+                      }
+                    ],
+                    as: "more_user_info"
+                }
+            },
+            {
+                $project: {
+                    _id: 0,
+                    id: 1,
+                    pw: 1,
+                    name: 1,
+                    reg_date: 1,
+                    more_user_info: { $arrayElemAt: ["$more_user_info", 0] }
+                  }
+            }
+        ])
+    } catch (e) {
+        if (e) throw e
+    }
+}
